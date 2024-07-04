3ba7ad18a297fc16f1a58bfef09d611192e7e513
youssef-fares <youssef.fares89@gmail.com>
2024-07-04 22:05:32 +0100

    fixed new operation, negative and '.' after equal

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let op = null;
 let displayValue = [];
 let opIndex = 0;
 let equalLock = true;
+let equalPressed = false;
 
 let operate = function (nb1, nb2, op) {
   console.log(op);
@@ -47,6 +48,20 @@ const display = document.querySelector(".display");
 
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
+    if (equalPressed === true && event.target.value === ".") {
+      return;
+    }
+
+    if (equalPressed === true) {
+      equalPressed = false;
+      displayValue = [];
+      opIndex = 0;
+      nb1 = null;
+      nb2 = null;
+      op = null;
+      console.log(equalPressed);
+    }
+
     displayValue.push(event.target.value);
     if (opIndex === 0 && displayValue.length < 10) {
       nb1 = parseFloat(displayValue.join(""));
@@ -58,23 +73,34 @@ numbers.forEach((number) => {
       display.textContent = nb2;
     }
     equalLock = false;
+    console.log(equalPressed);
+    // equalPressed = false;
   });
 });
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (event) => {
     if (nb2 !== null) {
-      nb1 = Math.round(operate(nb1, nb2, op) * 100000) / 100000;
+      nb1 = Math.round(operate(nb1, nb2, op) * 10000) / 10000;
       display.textContent = nb1;
     }
     op = event.target.value;
     opIndex++;
     displayValue = [];
     equalLock = true;
+    equalPressed = false;
+    console.log(equalPressed);
   });
 });
 
 negative.addEventListener("click", (event) => {
+  if (equalPressed === true) {
+    nb1 = -Math.round(operate(nb1, nb2, op) * 10000) / 10000;
+    display.textContent = nb1;
+    nb2 = null;
+    return;
+  }
+
   if (opIndex === 0) {
     nb1 = -nb1;
     display.textContent = nb1;
@@ -89,10 +115,12 @@ equal.addEventListener("click", (event) => {
     return;
   }
   if (equalLock === false) {
-    display.textContent = Math.round(operate(nb1, nb2, op) * 100000) / 100000;
+    display.textContent = Math.round(operate(nb1, nb2, op) * 10000) / 10000;
     equalLock = true;
     displayValue = [];
   }
+  equalPressed = true;
+  console.log(equalPressed);
 });
 
 clear.addEventListener("click", (event) => {
@@ -106,21 +134,34 @@ clear.addEventListener("click", (event) => {
 });
 
 del.addEventListener("click", (event) => {
+  if (equalPressed === true) {
+    return;
+  }
+
   if (
     opIndex === 0 &&
     (displayValue.length === 1 || displayValue.length === 0)
   ) {
     displayValue = [];
     display.textContent = 0;
+    nb1 = 0;
   } else if (opIndex === 0 && displayValue.length > 1) {
     displayValue.pop();
     nb1 = parseFloat(displayValue.join(""));
     display.textContent = nb1;
+  } else if (
+    opIndex === 1 &&
+    (displayValue.length === 1 || displayValue.length === 0)
+  ) {
+    displayValue = [];
+    display.textContent = 0;
+    nb2 = 0;
   } else {
     displayValue.pop();
     nb2 = parseFloat(displayValue.join(""));
     display.textContent = nb2;
   }
+  console.log(equalPressed);
 });
 
 if (displayValue.length > 9) {
